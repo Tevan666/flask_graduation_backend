a2409d41bdfd4dc0fcadb92b9081a0e4b381774b
tevan <1759377257@qq.com>
2022-04-21 19:06:04 +0800

    feat: 新增省份人数功能

---
+++ utils/people_count/count.py
@@ -0,0 +1,14 @@
+import json
+import re
+from os.path import dirname, join
+current_dir = dirname(__file__)
+file_path = join(current_dir, "province.json")
+with open(file_path, 'r', encoding="UTF-8") as load_file:
+  json_obj = json.load(load_file)
+# print(json_obj)
+count="6"
+square = "北京市"
+for item in json_obj["features"]:
+  if(re.findall(item["properties"]["name"], square)):
+    item["properties"]["adcode"]=count
+    print(item["properties"]["adcode"])
