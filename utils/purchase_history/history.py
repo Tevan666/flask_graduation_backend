fbc70a98242e23a01f54b4014594e6dc4e9d27f7
tevan <1759377257@qq.com>
2022-04-25 20:24:07 +0800

    fix: 修复history获取所有购买历史的方法

--- utils/purchase_history/history.py
+++ utils/purchase_history/history.py
@@ -1,13 +1,13 @@
 from modals import Demo_Login_Users, Animal, Purchase_history, Goods
 from exts import db
 from flask import Blueprint, jsonify
-from restApp import class_to_dict
 history_bp = Blueprint('history_bp', __name__, url_prefix='/api/')
 
 
 @history_bp.route('/history', methods = ['GET', 'POST'])
 def history():
-  history = db.session.query(Demo_Login_Users.name,Goods.type).filter(db.and_(Demo_Login_Users.userId == Purchase_history.user_id and Goods.goods_id == Purchase_history.goods_id))
+  #查询所有用户的购买历史
+  history = db.session.query(Demo_Login_Users.name,Goods.type).filter(Demo_Login_Users.userId == Purchase_history.user_id).filter(Goods.goods_id == Purchase_history.goods_id).all()
   arr = []
   print(history)
   for row in history:
@@ -15,4 +15,4 @@ def history():
     data['name']=row[0]
     data['type']=row[1]
     arr.append(data)
-  return jsonify(data=arr)
+  return jsonify(status=200,data=arr)
